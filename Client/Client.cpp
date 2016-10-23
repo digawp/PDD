d8173fc512ad15a84ff81495ce6775fef616f792
Diga Widyaprana <digawp@gmail.com>
2016-10-24 01:22:11 +0800

    Add method to generate private and public key (TODO: make a method that can import from other encoding

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -9,12 +9,19 @@
 #include <iostream>
 #include <sstream>
 
+#include "cryptopp/base64.h"
 #include "cryptopp/files.h"
 #include "cryptopp/filters.h"
 #include "cryptopp/hex.h"
+#include "cryptopp/integer.h"
 #include "cryptopp/nbtheory.h"
 #include "cryptopp/osrng.h"
+#include "cryptopp/rsa.h"
 #include "cryptopp/sha.h"
+#include "cryptopp/secblock.h"
+
+using CryptoPP::Integer;
+using CryptoPP::RSA;
 
 std::string hash_fn(const std::string& file_name) {
   std::string digest;
@@ -37,6 +44,28 @@ std::string hash_fn(const std::string& file_name) {
 }
 
 
+void generate_pub_pvt_key_pair() {
+  // InvertibleRSAFunction is used directly only because the private key
+  // won't actually be used to perform any cryptographic operation;
+  // otherwise, an appropriate typedef'ed type from rsa.h would have been used.
+  CryptoPP::AutoSeededRandomPool rng;
+  RSA::PrivateKey privkey;
+  privkey.Initialize(rng, 1024);
+
+  // With the current version of Crypto++, MessageEnd() needs to be called
+  // explicitly because Base64Encoder doesn't flush its buffer on destruction.
+  CryptoPP::Base64Encoder privkeysink(new CryptoPP::FileSink("privkey.txt"));
+  privkey.DEREncode(privkeysink);
+  privkeysink.MessageEnd();
+
+  // Suppose we want to store the public key separately,
+  // possibly because we will be sending the public key to a third party.
+  RSA::PublicKey pubkey(privkey);
+
+  CryptoPP::Base64Encoder pubkeysink(new CryptoPP::FileSink("pubkey.txt"));
+  pubkey.DEREncode(pubkeysink);
+  pubkeysink.MessageEnd();
+}
 
 Integer generate_r(Integer& n) {
   Integer r;
