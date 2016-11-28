8575e87b1ac8e5c751917bcd3603797bc43ea2c7
Diga Widyaprana <digawp@gmail.com>
2016-11-28 20:22:57 +0800

    Add accepting connection code on P

--- App/App.cpp
+++ App/App.cpp
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#include <cstdio>
 #include <iostream>
 #include "Enclave_u.h"
 #include "sgx_urts.h"
+
+// For networking
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #ifndef TRUE
 # define TRUE 1
@@ -10,6 +19,8 @@
 #ifndef FALSE
 # define FALSE 0
 #endif
+
+#define PORT "9876"
 
 /* Global EID shared by multiple threads */
 sgx_enclave_id_t global_eid = 0;
@@ -80,12 +91,104 @@ void ocall_print(const char* str) {
     printf("%s\n", str);
 }
 
+bool bind(struct addrinfo* servinfo_res, int* sock_fd) {
+    struct addrinfo* p;
+    for (p = servinfo_res; p != NULL; p = p->ai_next){
+        *sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (*sock_fd == -1) {
+            perror("server: socket");
+            continue;
+        }
+        int magic = 1;
+        if (setsockopt(*sock_fd, SOL_SOCKET, SO_REUSEADDR, &magic, sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+        if (bind(*sock_fd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(*sock_fd);
+            perror("server: bind");
+            continue;
+        }
+        break;
+    }
+    return p != NULL;
+}
+
 int main(int argc, char const *argv[]) {
     if (initialize_enclave() < 0) {
         std::cout << "Failed to initialize enclave" << std::endl;
+        return 1;
     }
 
     // Open connection, block to receive data
+    struct addrinfo* servinfo;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int addrinfo_status;
+    if ((addrinfo_status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        perror(gai_strerror(addrinfo_status));
+        return 1;
+    }
+
+    int sock_fd;
+    if(!bind(servinfo, &sock_fd)) {
+        std::cerr << "Server: Failed to bind." << std::endl;
+        return 1;
+    }
+    freeaddrinfo(servinfo);
+
+    if (listen(sock_fd, 10 /* backlog */) == -1) {
+        perror("listen");
+        return 1;
+    }
+    std::cout << "Listening on port " << PORT << std::endl;
+
+    int new_conn_fd;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    struct sigaction sa;
+
+    while(true) {
+        sin_size = sizeof(their_addr);
+        new_conn_fd = accept(sock_fd, (struct sockaddr*)&their_addr, &sin_size);
+
+        if (new_conn_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        // sockaddr magic: http://beej.us/guide/bgnet/output/html/multipage/sockaddr_inman.html
+        char client_addr[INET6_ADDRSTRLEN];
+        if (their_addr.ss_family == AF_INET6) {
+            inet_ntop(AF_INET6, (struct sockaddr_in6*)&their_addr, client_addr, INET6_ADDRSTRLEN);
+        } else {
+            inet_ntop(AF_INET, (struct sockaddr_in*)&their_addr, client_addr, INET6_ADDRSTRLEN);
+        }
+        std::cout << client_addr << " connected." << std::endl;
+
+        if (!fork()) { // is child process
+            // Child doesn't need to listen to new connections.
+            close(sock_fd);
+            char buffer[1024];
+            int bytes_recv = 0;
+            while((bytes_recv = recv(new_conn_fd, buffer, 1024, 0)) > 0) {
+                buffer[bytes_recv] = '\0';
+                std::cout << buffer;
+                if (send(new_conn_fd, buffer, strlen(buffer), 0) == -1) {
+                    perror("send");
+                }
+            }
+            std::cout << "Close" << std::endl;
+            close(new_conn_fd);
+            exit(0);
+        }
+        // Parent doesn't need to handle the new connection
+        close(new_conn_fd);
+    }
 
     // Data received, sign with pvt key
 
