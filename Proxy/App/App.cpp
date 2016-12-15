061e711b0b6c035c00efe0c8f6c9c489965dc8e7
Diga Widyaprana <digawp@gmail.com>
2016-12-15 16:48:18 +0800

    Restructure Proxy to treat Sealing as managed edl static library, and sgx_utils as normal managed static library

--- Proxy/App/App.cpp
+++ Proxy/App/App.cpp
@@ -8,6 +8,7 @@
 
 #include "Blind.h"
 #include "Ocall.h"
+#include "sgx_utils/sgx_utils.h"
 
 // For networking
 #include <arpa/inet.h>
@@ -17,14 +18,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
-#ifndef TRUE
-# define TRUE 1
-#endif
-
-#ifndef FALSE
-# define FALSE 0
-#endif
 
 #define PORT "9876"
 #define CHUNK_SIZE 4096
@@ -42,67 +35,6 @@ sgx_enclave_id_t global_eid = 0;
 
 // Chunk number
 uint64_t global_chunk_no = 0;
-
-/* Check error conditions for loading enclave */
-void print_error_message(sgx_status_t ret)
-{
-    std::cout << ret << std::endl;
-}
-
-/* Initialize the enclave:
- *   Step 1: try to retrieve the launch token saved by last transaction
- *   Step 2: call sgx_create_enclave to initialize an enclave instance
- *   Step 3: save the launch token if it is updated
- */
-int initialize_enclave() {
-    char* token_path = "launch_token_file";
-    sgx_launch_token_t token = {0};
-    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
-    int updated = 0;
-
-    /* Step 1: try to retrieve the launch token saved by last transaction
-     *         if there is no token, then create a new one.
-     */
-    /* try to get the token saved in $HOME */
-    FILE* fp = fopen(token_path, "rb");
-    if (fp == NULL && (fp = fopen(token_path, "wb")) == NULL) {
-        printf("Warning: Failed to create/open the launch token file \"%s\".\n", token_path);
-    }
-
-    if (fp != NULL) {
-        /* read the token from saved file */
-        size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
-        if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
-            /* if token is invalid, clear the buffer */
-            memset(&token, 0x0, sizeof(sgx_launch_token_t));
-            printf("Warning: Invalid launch token read from \"%s\".\n", token_path);
-        }
-    }
-    /* Step 2: call sgx_create_enclave to initialize an enclave instance */
-    /* Debug Support: set 2nd parameter to 1 */
-    ret = sgx_create_enclave("enclave.signed.so", SGX_DEBUG_FLAG, &token, &updated, &global_eid, NULL);
-    if (ret != SGX_SUCCESS) {
-        print_error_message(ret);
-        if (fp != NULL) fclose(fp);
-        return -1;
-    }
-
-    /* Step 3: save the launch token if it is updated */
-    if (updated == FALSE || fp == NULL) {
-        /* if the token is not updated, or file handler is invalid, do not perform saving */
-        if (fp != NULL) fclose(fp);
-        return 0;
-    }
-
-    /* reopen the file with write capablity */
-    fp = freopen(token_path, "wb", fp);
-    if (fp == NULL) return 0;
-    size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
-    if (write_num != sizeof(sgx_launch_token_t))
-        printf("Warning: Failed to save launch token to \"%s\".\n", token_path);
-    fclose(fp);
-    return 0;
-}
 
 bool bind(struct addrinfo* servinfo_res, int* sock_fd) {
     struct addrinfo* p;
@@ -191,11 +123,9 @@ bool unseal_and_reconstruct_file(const std::string& file_name) {
         chunk_file.close();
 
         char unsealed_buf[CHUNK_SIZE];
-        int unseal_success;
-        sgx_status_t status = unseal(global_eid, &unseal_success, (sgx_sealed_data_t*)sealed_buf, sealed_size, (uint8_t*)unsealed_buf, CHUNK_SIZE);
-        if (status != SGX_SUCCESS || !unseal_success) {
-            DEBUG_LOG("Sealing failed. Aborting.");
-            print_error_message(status);
+        sgx_status_t ecall_success;
+        sgx_status_t status = unseal(global_eid, &ecall_success, (sgx_sealed_data_t*)sealed_buf, sealed_size, (uint8_t*)unsealed_buf, CHUNK_SIZE);
+        if (!is_ecall_successful(status, "Unsealing failed. Aborting.", ecall_success)) {
             return false;
         }
 
@@ -220,11 +150,9 @@ bool unseal_and_reconstruct_file(const std::string& file_name) {
 bool seal_and_store(std::ofstream& file, char* buffer) {
     size_t sealed_size = sizeof(sgx_sealed_data_t) + CHUNK_SIZE;
     char sealed_buf[sealed_size];
-    int seal_success;
-    sgx_status_t status = seal(global_eid, &seal_success, (uint8_t*)buffer, CHUNK_SIZE, (sgx_sealed_data_t*)sealed_buf, sealed_size);
-    if (status != SGX_SUCCESS || !seal_success) {
-        DEBUG_LOG("Sealing failed. Aborting.");
-        print_error_message(status);
+    sgx_status_t ecall_success;
+    sgx_status_t status = seal(global_eid, &ecall_success, (uint8_t*)buffer, CHUNK_SIZE, (sgx_sealed_data_t*)sealed_buf, sealed_size);
+    if (!is_ecall_successful(status, "Sealing failed. Aborting.", ecall_success)) {
         return false;
     }
     file.write(sealed_buf, sealed_size);
@@ -232,7 +160,7 @@ bool seal_and_store(std::ofstream& file, char* buffer) {
 }
 
 int main(int argc, char const *argv[]) {
-    if (initialize_enclave() < 0) {
+    if (initialize_enclave(&global_eid, "enclave.token", "enclave.signed.so") < 0) {
         std::cout << "Failed to initialize enclave" << std::endl;
         return 1;
     }
